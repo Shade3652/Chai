8c67b471aebabaf2b939454a6fdea88a8dff53a9
Shadewalker3652 <122281648+Shade3652@users.noreply.github.com>
2024-01-13 17:56:08 +0000

    MAJORLY fix lexer_advance script

--- Chai.py
+++ Chai.py
@@ -3,9 +3,9 @@
 ##########
 #TOKENS
 ##########
-global lexer_current_cha; lexer_current_cha = None
+lexer_char_num = -1
+lexer_current_cha = None
 tokens = []
-lexer_char_num = 0
 
 TT_INT = "INT"
 TT_FLOAT = "FLOAT"
@@ -18,46 +18,45 @@ TT_RPAR = "RPAR"
 
 class Token():
     def init():
-            lexer_advance()
+            Token.lexer_advance()
             
 
     def tokenize(fn,ln):
         while lexer_current_cha != None:
             if lexer_current_cha in ' \t':
-                lexer_advance()
+                Token.lexer_advance()
             elif lexer_current_cha in "1234567890":
-                tokens.append(Token.make_number())
+                print(Token.make_number())
             elif lexer_current_cha == "+":
-                tokens.append(TT_PLUS)
-                lexer_advance()
+                print(TT_PLUS)
+                Token.lexer_advance()
             elif lexer_current_cha == "-":
-                tokens.append(TT_MINUS)
-                lexer_advance()
+                print(TT_MINUS)
+                Token.lexer_advance()
             elif lexer_current_cha == "*":
-                #tokens.append(TT_MUL)
-                lexer_advance()
+                print(TT_MUL)
+                Token.lexer_advance()
             elif lexer_current_cha == "/":
-                #tokens.append(TT_DIV)
-                lexer_advance()
+                print(TT_DIV)
+                Token.lexer_advance()
             elif lexer_current_cha == "(":
-                #tokens.append(TT_LPAR)
-                lexer_advance()
+                print(TT_LPAR)
+                Token.lexer_advance()
             elif lexer_current_cha == ")":
-                #tokens.append(TT_RPAR)
-                lexer_advance()
+                print(TT_RPAR)
+                Token.lexer_advance()
             elif lexer_current_cha == " ":
                 pass
-                #lexer_advance()
+                #Token.lexer_advance()
             else:
-                #lexer_char = lexer_current_cha  
-                lexer_advance()
+                Token.lexer_advance()
                 Errors.BadCharacter(ln,fn)
 
 
 
         return tokens, None
     
-    def make_number(self):
+    def make_number():
         num_str = ''
         dot_count = 0
 
@@ -67,15 +66,18 @@ class Token():
                 dot_count += 1
                 num_str += '.'
             else:
-                num_str += lexer_char_num
+                num_str += lexer_current_cha
 
-            lexer_advance()
+            Token.lexer_advance()
 
-def lexer_advance():
-    if lexer_char_num < len(text):
-        lexer_current_cha = text[lexer_char_num]
-    else:
-        lexer_current_cha = None
+    def lexer_advance():
+        global lexer_char_num, lexer_current_cha
+        if lexer_char_num < len(text):
+            lexer_char_num = lexer_char_num + 1
+            lexer_current_cha = text[lexer_char_num]
+
+        else:
+            lexer_current_cha = None
 
 ##########
 #ERRORS
@@ -91,13 +93,12 @@ class Errors():
 ##########
             
 class Lexer():
-    def __init__(filen,txt,ln):
+    def __init__(filen,ln):
         global fn
         fn = filen
-        global text
-        text = txt
         Token.init()
-        Token.tokenize(fn,ln)
+        Token.tokenize(filen,ln)
+        print(lexer_char_num, lexer_current_cha)
         print(tokens)
 
 
@@ -114,4 +115,4 @@ class Lexer():
         
 while True:
   text = input("Chai > ")
-  Lexer.__init__("File",text,1)
+  Lexer.__init__("File",1)
